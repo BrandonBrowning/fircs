6c04ba80159042df2b39f7fe9807280cfa4c7e4c
Brandon Browning <brandonbrowning@live.com>
2014-11-08 14:00:09 -0500

    Added matches style tests and regression on ident @ sign

--- FIRCS.Test/CommonTest.fs
+++ FIRCS.Test/CommonTest.fs
@@ -4,6 +4,9 @@ module CommonTest
 open FParsec
 open NUnit.Framework
 
+let assertEqual<'a> (left: 'a) (right: 'a) = Assert.AreEqual(left, right)
+let assertNotEqual<'a> (left: 'a) (right: 'a) = Assert.AreNotEqual(left, right)
+
 let assertParses p str = 
     match runParserOnString (p .>> eof) () "test" str with
         | Failure(text, error, state) -> Assert.Fail text
@@ -13,3 +16,13 @@ let assertNotParses p str =
     match runParserOnString (p .>> eof) () "test" str with
         | Failure(text, error, state) -> printfn "%A" text
         | Success(result, state, position) -> sprintf "Expected parser to fail; got %A" result |> Assert.Fail
+
+let assertParseMatches p str o = 
+    match runParserOnString (p .>> eof) () "test" str with
+        | Failure(text, error, state) -> Assert.Fail text
+        | Success(result, state, position) -> assertEqual result o
+
+let assertParseNotMatches p str o = 
+    match runParserOnString (p .>> eof) () "test" str with
+        | Failure(text, error, state) -> Assert.Fail text
+        | Success(result, state, position) -> assertNotEqual result o
--- FIRCS.Test/TestParse.fs
+++ FIRCS.Test/TestParse.fs
@@ -6,5 +6,10 @@ open Parser
 open FParsec
 open NUnit.Framework
 
-[<Test>] let ``parsePrefixNick pass> foobar!~foobar@cloak-34A93E1F.cl.ri.cox.net``() =
-    assertParses parsePrefixNick "foobar!~foobar@cloak-34A93E1F.cl.ri.cox.net"
+[<Test>] let ``(parsePrefixNick match) foobar!~foobar@example.com``() =
+    assertParseMatches parsePrefixNick "foobar!~foobar@example.com"
+        ("foobar", Some "~foobar", Some "example.com")
+
+// ident shouldn't have @, and host can't have an @
+[<Test>] let ``(parsePrefixNick fail) foobar!foo@bar@cloak-34A93E1F.cl.ri.cox.net``() =
+    assertNotParses parsePrefixNick "foobar!~foo@bar@cloak-34A93E1F.cl.ri.cox.net"
