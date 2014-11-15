603aa2b9cca96d28ec58332e9a8652a0474cbc7a
Brandon Browning <brandonbrowning@live.com>
2014-11-14 22:46:55 -0500

    Refactored bot message logic and added echoing when named

--- FIRCS.Bot/Program.fs
+++ FIRCS.Bot/Program.fs
@@ -1,6 +1,7 @@
 ﻿
 module Program
 open FParsec
+open Grammar
 open Parser
 
 open System
@@ -9,14 +10,39 @@ open System.Net.Sockets
 open System.Text
 open System.Threading
 
-let getIrcSocket (host: string): Socket option =
+let messageToString (message: Message): string =
+    let prefix, command, args = message
+
+    let prefixStr = match prefix with
+        | None -> ""
+        | Some(prefix) ->
+            let body = match prefix with
+                | PrefixTarget(str) -> str
+                | PrefixServer(str) -> str
+                | PrefixNick(nick, user, host) -> 
+                    let user_str = match user with None -> "" | Some(x) -> sprintf "!%A" x
+                    let host_str = match host with None -> "" | Some(x) -> sprintf "@%A" x
+                    nick + user_str + host_str
+            sprintf ":%s " body
+
+    let argsStr =
+        let rec argsStr' args (acc: string list) = 
+            match args with
+                | [] -> if acc.Length = 0 then "" else " " + String.Join(" ", acc |> List.rev)
+                | h::[] -> argsStr' [] ((":" + h)::acc)
+                | h::t -> argsStr' t (h::acc)
+        argsStr' args []
+
+    sprintf "%s%s%s" prefixStr command argsStr
+
+let makeIRCSocket (host: string): Socket option =
     let rec getIrcSocket' (address: IPAddress) =
         let endpoint = new IPEndPoint(address, 6667)
         let socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp)
         socket.Connect(endpoint)
         socket.NoDelay <- true
         if socket.Connected then Some socket else None
-        
+
     let dns = Dns.GetHostEntry(host)
     Seq.tryPick getIrcSocket' dns.AddressList
 
@@ -25,44 +51,81 @@ let like (left: string) (right: string) = left.Equals(right, StringComparison.In
 let buffer = Array.create 4048 0uy
 
 let getSplitArray = Array.create 1 "\r\n"
-let get (socket: Socket): string array =
+let getSocketLines (socket: Socket): string array =
     let length = socket.Receive(buffer)
     let text = Encoding.UTF8.GetString(buffer, 0, length)
-    let result = text.Split(getSplitArray, StringSplitOptions.RemoveEmptyEntries)
+    text.Split(getSplitArray, StringSplitOptions.RemoveEmptyEntries)
 
-    for r in result do
-        Console.WriteLine("< {0}", r)
-
-    result
-
-let send (socket: Socket) (msg: string) =
-    Console.WriteLine("> {0}", msg)
-    let payload = msg + "\r\n"
+let sendSocketLine (socket: Socket) (str: string) =
+    let payload = str + "\r\n"
     payload |> Encoding.UTF8.GetBytes |> socket.Send |> ignore
 
+let getSocketMessages (socket: Socket): Message seq =
+    seq {
+        for line in getSocketLines socket do
+            match runParserOnString parseMessage () "irc input" line with
+                | Failure(text, error, state) ->
+                    printfn "! Failed to parse the following..\n%s" text
+                | Success(result, state, position) ->
+                    printfn "< %s" line
+                    yield result
+    }
+
+let sendSocketMessage (socket: Socket) (message: Message) =
+    let line = messageToString message
+    printfn "> %s" line
+    line  + "\r\n" |> sendSocketLine socket
+
+let outMessage (command: string) (args: string list): Message = (None, command.ToUpper(), args)
+let userMessage user = outMessage "user" [user; "localhost"; "localhost"; "chickensalad's bot"]
+let nickMessage nick = outMessage "nick" [nick]
+let pongMessage arg = outMessage "pong" [arg]
+let joinMessage channel = outMessage "join" [channel]
+let privMessage target message = outMessage "privmsg" [target; message]
+let channelMessage channel message = outMessage "privmsg" [channel; message]
+
+let nick = "sirsalad"
+let user = nick
+let channel = "#uakroncs"
+
+let processMessage (message: Message): Message seq =
+    seq {
+        let (prefixOpt, command, args) = message
+
+        if like command "ping" then
+            yield pongMessage args.[0]
+        else if like command "mode" then
+            yield joinMessage channel
+        else if like command "privmsg" then
+            match prefixOpt with
+                | Some(PrefixNick(from_nick, _, _)) ->
+                    let target = args.[0]
+                    let message = args.[1]
+
+                    if target = nick then
+                        yield privMessage from_nick message
+                    else if target = channel then
+                        let nick_prefix = sprintf "%s:" nick
+                        if message.StartsWith(nick_prefix) then
+                            let response = message.Substring(nick_prefix.Length).Trim()
+                            yield sprintf "%s: %s" from_nick response |> channelMessage channel
+                | _ -> ()
+    }
+
 [<EntryPoint>]
-let main argv = 
-    match getIrcSocket "concrete.slashnet.org" with
+let main argv =
+    match makeIRCSocket "concrete.slashnet.org" with
         | None -> failwith "Could not get a socket"
         | Some(socket) ->
-            send socket "USER sirsalad localhost localhost :chickensalad's bot"
-            send socket "NICK sirsalad"
+            let send = sendSocketMessage socket
+            let gets() = getSocketMessages socket
+
+            userMessage user |> send
+            nickMessage nick |> send
 
             let rec loop() =
-                let commands = get socket
-                for command in commands do
-                    printfn ""
-                    match runParserOnString parseMessage () "test" command with
-                        | Failure(text, error, state) ->
-                            printfn "! Failed to parse the following..\n%s" text
-                        | Success(result, state, position) ->
-                            let prefix, command, parameters = result
-                            if like command "ping" then
-                                sprintf "PONG :%s" parameters.[0] |> send socket
-                            else if like command "mode" then
-                                send socket "JOIN #uakroncs"
+                gets() |> Seq.map processMessage |> Seq.concat |> Seq.iter send
                 loop()
-            loop()
 
-    Console.ReadLine() |> ignore
+            loop()
     0
